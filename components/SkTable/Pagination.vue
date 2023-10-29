7876c2ee53720923e0f3c9a8f7c4c3df81262f0e
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-10-29 00:21:50 +0000

    Style pagination

--- components/SkTable/Pagination.vue
+++ components/SkTable/Pagination.vue
@@ -14,11 +14,22 @@ function prevPage() {
 function nextPage() {
     emit('onPage', props.pagination.page + 1)
 }
+
+function goToPage(page: number) {
+    emit('onPage', page)
+}
 </script>
 
 <template>
-    <section>
-        <button @click="prevPage">Prev</button>
-        <button @click="nextPage">Next</button>
+    <section class="sk-pagination">
+        <button @click="goToPage(1)">«</button>
+        <button @click="prevPage">‹</button>
+        
+        <button v-for="item in pagination.total_pages" @click="goToPage(item)">
+            {{ item }}
+        </button>
+
+        <button @click="nextPage">›</button>
+        <button @click="goToPage(pagination.total_pages)">»</button>
     </section>
 </template>
