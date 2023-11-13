9cb119887c1266b574c5819de0de5bb737896670
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-11-13 01:47:57 +0000

    Prevent send search in useTableData

--- composables/useTableData.ts
+++ composables/useTableData.ts
@@ -1,15 +1,20 @@
 export async function useTableData<T>(path: string) {
+    // data
     const page = ref(1)
     const search = ref('')
 
     const key = path.split('/').pop() as string
 
+    // computed
+    const searchPurified = computed(() => search.value || undefined)
+
+    // methods
     const { data, refresh, pending } = useFetch<ITable<T>>(path, 
         {
             key,
             query: {
                 page,
-                search
+                search: searchPurified
             }
         }
     )
