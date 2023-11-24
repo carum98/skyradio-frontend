cc44b228bfbbf4840f3d30419802203ae3515909
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-11-24 03:30:00 +0000

    Seller page

---
+++ pages/sellers/[code].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+definePageMeta({
+    name: 'sellers-profile'
+})
+
+const route = useRoute()
+
+const { data } = await useFetch(`/api/sellers/${route.params.code}`)
+</script>
+
+<template>
+    <main>
+        {{ data }}
+    </main>
+</template>
