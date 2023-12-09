84c60ddd7c324ebc6bf80c07184c12a2b4a47c48
Carlos Umaña Acevedo <carumtito@gmail.com>
2023-12-09 01:18:52 +0000

    Refactor add-radios

--- views/add-radios.vue
+++ views/add-radios.vue
@@ -14,12 +14,25 @@ const radios = ref<IRadio[]>([])
 
 // methods
 async function send() {
-    await $fetch(`/api/clients/${props.client.code}/radios`, {
+    const addPromise = $fetch(`/api/clients/${props.client.code}/radios`, {
         method: 'POST',
         body: {
            radios_codes: radios.value.map((radio) => radio.code)
         }
     })
+
+    const updatePromise = radios.value.map((radio) => $fetch(`/api/radios/${radio.code}`, {
+        method: 'PUT',
+        body: {
+            name: radio.name,
+            sim_code: radio.sim?.code
+        }
+    }))
+
+    await Promise.allSettled([
+        addPromise, 
+        ...updatePromise
+    ])
 
     emits('refresh')
     emits('close')
@@ -39,16 +52,30 @@ watch(radio, (value) => {
 </script>
 
 <template>
-    <form class="sk-form" @submit.prevent="send" style="width: 300px;">
-        <label>Buscar</label>
+    <form class="sk-form" @submit.prevent="send" style="width: 750px;">
         <SelectRadio 
             v-model="radio"
         />
 
-        <label>Radios seleccionados</label>
         <ul class="list-radios-form">
-            <li v-for="radio in radios">
+            <li v-for="radio in radios" class="add-radio-item">
+                <input
+                    type="text"
+                    class="sk-input"
+                    placeholder="Nombre del radio"
+                    v-model="radio.name" 
+                />
+
                 {{ radio.imei }}
+
+                <span v-if="radio.model" class="sk-link">
+                    <span class="badge-color" :style="{ backgroundColor: radio.model.color }"></span>
+                    {{ radio.model.name }}
+                </span>
+
+                <SelectSim
+                    v-model="radio.sim"
+                />
 
                 <button type="button" @click="removeRadio(radio)">
                     <IconsTrashBin />
@@ -61,3 +88,11 @@ watch(radio, (value) => {
         </button>
     </form>
 </template>
+
+<style>
+.add-radio-item {
+    display: grid !important;
+    grid-template-columns: 235px 1fr 70px 1fr 35px;
+    gap: 20px;
+}
+</style>
