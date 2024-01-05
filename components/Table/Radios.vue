1e6ad82e1f0fa18c08607a05e45928b743ed9c64
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-01-05 23:50:45 +0000

    Add IMEI copy button

--- components/Table/Radios.vue
+++ components/Table/Radios.vue
@@ -135,6 +135,10 @@ function openAdd(client: IClient | null) {
         }
     })
 }
+
+function copy(value: string) {
+    navigator.clipboard.writeText(value)
+}
 </script>
 
 <template>
@@ -167,6 +171,15 @@ function openAdd(client: IClient | null) {
                 :openAdd="openAdd"
             />
 
+        </template>
+
+        <template #cell(imei)="{ value }">
+            <span class="imei-text">
+                {{ value }}
+                <button @click.stop="copy(value)">
+                    <svg width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M18 2H9c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2m0 14H9V4h9zM3 15v-2h2v2zm0-5.5h2v2H3zM10 20h2v2h-2zm-7-1.5v-2h2v2zM5 22c-1.1 0-2-.9-2-2h2zm3.5 0h-2v-2h2zm5 0v-2h2c0 1.1-.9 2-2 2M5 6v2H3c0-1.1.9-2 2-2"/></svg>
+                </button>
+            </span>
         </template>
 
         <template #cell(model)="{ value }">
