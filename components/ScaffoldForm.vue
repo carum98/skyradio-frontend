ad0331efd26a7e3ce0df4286e54504f947649856
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-01-06 03:07:40 +0000

    Add toast notifications for success and error messages

--- components/ScaffoldForm.vue
+++ components/ScaffoldForm.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts" generic="T extends SkFormData">
 import { SkFormData } from '@utils/form-data'
+const toast = useToast()
 
 const props = defineProps<{
     form: T
@@ -20,21 +21,36 @@ const disabled = computed<boolean>(() => !props.form.isValid || props.form.isDir
 
 // methods
 async function onSubmitted() {
-    const path = props.form.isEditing
-        ? props.pathUpdate.replace(/\/:\w+/, `/${props.form.code}`)
-        : props.pathCreate
+    try {
+        const path = props.form.isEditing
+            ? props.pathUpdate.replace(/\/:\w+/, `/${props.form.code}`)
+            : props.pathCreate
     
-    const method = props.form.isEditing
-        ? 'PUT' 
-        : 'POST'
+        const method = props.form.isEditing
+            ? 'PUT' 
+            : 'POST'
 
-    await $fetch(path, {
-        method,
-        body: props.form.toParams(),
-    })
+        await $fetch(path, {
+            method,
+            body: props.form.toParams(),
+        })
 
-    emits('refresh')
-    emits('close')
+        toast.open({
+            title: 'Exito!!',
+            message: props.form.isEditing ? 'Editado correctamente' : 'Creado correctamente',
+            type: 'success',
+        })
+
+        emits('refresh')
+        emits('close')
+    } catch (error) {
+        console.error(error)
+        toast.open({
+            title: 'Error!!',
+            message: props.form.isEditing ? 'Error al editar' : 'Error al crear',
+            type: 'error',
+        })
+    }
 }
 </script>
 
