387a72fcecf4d7ac31c88ee0c607283b71e2e64b
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-01-13 04:05:06 +0000

    Fix API fetch configuration

--- server/utils/apiFetch.ts
+++ server/utils/apiFetch.ts
@@ -4,7 +4,7 @@ import { H3Event, createError } from 'h3'
 const config = useRuntimeConfig()
 
 const apiFetch = ofetch.create({ 
-    
+    baseURL: config.apiBaseUrl, 
     headers: {
         'Content-Type': 'application/json'
     }
@@ -13,18 +13,20 @@ const apiFetch = ofetch.create({
 export const useApiFetch = async (event: H3Event, request: RequestInfo, options?: FetchOptions) => {
     const token = event.context.session?.auth?.token
 
+    console.log('useApiFetch token', token)
+
     if (!token) {
         await useLogout(event)
     }
 
     return apiFetch(request, {
-        baseURL: config.apiBaseUrl,
         ...options,
         headers: {
             'Authorization': `Bearer ${token}`
         },
         async onResponseError({ response, error }) {
-            console.log('onResponseError', response, error)
+            console.log('onResponseError response', response)
+            console.log('onResponseError error', error)
             if (response.status === 401) {
                 await useLogout(event)
             } else {
