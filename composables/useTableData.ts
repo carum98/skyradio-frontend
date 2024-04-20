644a5494e28b866efbc6665b702cdd228f394a5c
Carlos Umaña Acevedo <carumtito@gmail.com>
2024-04-20 19:23:49 +0000

    Fix, generate unique key to table request

--- composables/useTableData.ts
+++ composables/useTableData.ts
@@ -1,4 +1,4 @@
-import { parseURL } from 'ufo'
+import { parseURL, parseQuery } from 'ufo'
 
 export async function useTableData<T>(path: string) {
     const route = useRoute()
@@ -8,7 +8,24 @@ export async function useTableData<T>(path: string) {
     const search = ref(route.query.search as string || '')
     const query = ref<Record<string, any>>({})
 
-    const key = parseURL(path).pathname.split('/').pop()
+    const url = parseURL(path)
+
+    let key = url.pathname.split('/').pop()
+
+    // Modify unique key, if the url has query params
+    if (url.search) {
+        const parsed = parseQuery(url.search)
+
+        // Find a query params with a key containing 'code' to concatenate
+        // to the key
+        const code = Object.keys(parsed).find(key => key.includes('code'))
+
+        if (code) {
+            key += `-${parsed[code]}`
+        }
+    }
+
+    console.log(key)
 
     // computed
     const searchPurified = computed(() => search.value || undefined)
